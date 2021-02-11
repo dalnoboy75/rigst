e28d8a2b3acd6a8dee8060d6d745ec743060a452
intelboy228 <novikovivan005@gmail.com>
2021-02-11 19:55:46 +0300

    1

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id = "app">
+        <div id = "nav">
+            <router-link to ="/"> Home </router-link>
+            <router-link to ="/Login"> Login </router-link>
+            <router-link to ="/about"> About </router-link>
+        </div>
+        <router-view @auth = "authStart" />
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                user: '',
+            }
+        },
+        methods: {
+            authStart($data) {
+                this.user = $data;
+            }
+        }
+    }
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <input v-model = "login" type = "text" placeholder = "Логин">
+        <input v-model = "password" type = "password" placeholder = "Пароль">
+        <input type = "submit" value = "Войти" @click = "auth">
+    </div>
+<template>
+
+<script>
+    import axios from 'axios'
+    
+    export default {
+        data(){
+            return{
+                login: '',
+                password: '',
+            }
+        },
+        methods: {
+            auth(){
+                axios.get('http://37.77.104.246/api/jsonstorage/?id=0fbf42f8761476dae7507c99477f9bcb')
+                .then(res =>{
+                    console.log(res.data)
+                    
+                    for (let i=0; i< res.data.lengthl i++){
+                        if (res.data[i].password == this.password && res.data[i].login == this.login) {
+                            this.$router.push('/users/${res.data[i].login}')
+                            this.$emit('auth', res.data[i])
+                        }
+                    }
+                })
+            }
+        
+        }
+    
+    }
+</script>
